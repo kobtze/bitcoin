7222cd92ed42b4b29680f26bdbe2fc3d1fc12587
Kobi Donner <kobibo@gmail.com>
2020-09-07 23:24:44 +0300

    Updates

---
+++ precache-manifest.80eea32c11ff22db4fafcd50a6c6c534.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdc51fe57f31888b787ca759d42f4cb",
+    "url": "/bitcoin/index.html"
+  },
+  {
+    "revision": "f3c92f42bd369db1c593",
+    "url": "/bitcoin/static/css/2.2b22ad79.chunk.css"
+  },
+  {
+    "revision": "5ebe3fda9b2a0a1c9d20",
+    "url": "/bitcoin/static/css/main.c79cc758.chunk.css"
+  },
+  {
+    "revision": "f3c92f42bd369db1c593",
+    "url": "/bitcoin/static/js/2.5916f6ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bitcoin/static/js/2.5916f6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ebe3fda9b2a0a1c9d20",
+    "url": "/bitcoin/static/js/main.25d84548.chunk.js"
+  },
+  {
+    "revision": "e785d8960af0004feb6f",
+    "url": "/bitcoin/static/js/runtime-main.da43fe6a.js"
+  }
+]);
